0b88acb81e74a47ee80b9fc32848657793a70eb5
Stefan Ivković <ivkovics@outlook.com>
2025-05-01 17:39:49 +0200

    feat(user): add email verification token entity

---
+++ AppointMate/src/Services/User/AppointMate.User.Domain/User/Entities/EmailVerificationToken.cs
@@ -0,0 +1,31 @@
+﻿using AppointMate.User.Domain.Abstractions;
+using AppointMate.User.Domain.User.ValueObjects;
+
+namespace AppointMate.User.Domain.User.Entities
+{
+    public sealed class EmailVerificationToken : Entity<VerificationTokenId>
+    {
+        private const int TokenExpiresInMinutes = 60;
+
+        private EmailVerificationToken(DateTime createdAt) => CreatedAt = createdAt;
+
+        public static EmailVerificationToken Create(DateTime createdAt = default)
+        {
+            if (createdAt == default)
+            {
+                createdAt = DateTime.UtcNow;
+            }
+
+            return new EmailVerificationToken(createdAt);
+        }
+
+        public bool IsValid(VerificationTokenId id) => Id == id && !IsExpired();
+
+        private bool IsExpired()
+        {
+            DateTime currentDateTime = DateTime.UtcNow;
+
+            return currentDateTime > CreatedAt.Value.AddMinutes(TokenExpiresInMinutes);
+        }
+    }
+}
