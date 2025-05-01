f98e3bb79f10b1e6bfc87a62114a50a162d1bfd8
Stefan Ivković <ivkovics@outlook.com>
2025-05-01 17:39:36 +0200

    feat(user): add verification token id value object

---
+++ AppointMate/src/Services/User/AppointMate.User.Domain/User/ValueObjects/VerificationTokenId.cs
@@ -0,0 +1,21 @@
+﻿namespace AppointMate.User.Domain.User.ValueObjects
+{
+    public sealed class VerificationTokenId
+    {
+        public Guid Value { get; }
+
+        private VerificationTokenId(Guid value) => Value = value;
+
+        public static VerificationTokenId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (value == Guid.Empty)
+            {
+                throw new ArgumentException("VerificationTokenId can not be null", nameof(value));
+            }
+
+            return new VerificationTokenId(value);
+        }
+    }
+}
