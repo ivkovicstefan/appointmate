2ea14b6f8533eeaedfa90dad4870af177a86c4bd
Stefan Ivković <ivkovics@outlook.com>
2025-05-01 15:34:00 +0200

    feat(user): add full name value object

---
+++ AppointMate/src/Services/User/AppointMate.User.Domain/User/ValueObjects/FullName.cs
@@ -0,0 +1,32 @@
+﻿using AppointMate.User.Domain.Abstractions;
+
+namespace AppointMate.User.Domain.User.ValueObjects
+{
+    public sealed class FullName : ValueObject
+    {
+        public string FirstName { get; } = default!;
+        public string LastName { get; } = default!;
+
+        private FullName(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public static FullName Of(string firstName, string lastName)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(firstName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(lastName);
+
+            return new FullName(firstName, lastName);
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return FirstName;
+            yield return LastName;
+        }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+    }
+}
