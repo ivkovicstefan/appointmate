963eabf0aa67d39163bddfd15247e2cfdcbc139b
Stefan Ivković <ivkovics@outlook.com>
2025-05-01 16:04:20 +0200

    feat(user): add email value object

---
+++ AppointMate/src/Services/User/AppointMate.User.Domain/User/ValueObjects/Email.cs
@@ -0,0 +1,35 @@
+﻿using AppointMate.User.Domain.Abstractions;
+using System.Text.RegularExpressions;
+
+namespace AppointMate.User.Domain.User.ValueObjects
+{
+    public sealed class Email : ValueObject
+    {
+        private const string EmailAddressValidationPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        public string EmailAddress { get; } = default!;
+
+        private Email(string emailAddress)
+        {
+            EmailAddress = emailAddress;
+        }
+
+        public static Email Of(string emailAddress)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+
+            if (!Regex.IsMatch(emailAddress, EmailAddressValidationPattern))
+            {
+                throw new ArgumentException("Invalid Email Format.", nameof(emailAddress));
+            }
+
+            return new Email(emailAddress);
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return EmailAddress.ToLowerInvariant();
+        }
+
+        public override string ToString() => EmailAddress;
+    }
+}
